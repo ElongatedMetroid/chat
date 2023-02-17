4994b3ef985d71699fa91afde0ac113725bf55e0
ElongatedMetroid <78760239+NateNoNameSOFT@users.noreply.github.com>
2023-02-16 21:26:23 -0800

    test sending messages

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpStream;
+
+use chat_core::{write::ChatWriter, message::{Message, Value}};
+
 fn main() {
-    println!("Hello, world!");
+    let mut stream = TcpStream::connect("127.0.0.1:1234").unwrap();
+
+    stream.write_message(Message {
+        payload: Value::Integer(7) 
+    }).unwrap();
 }
