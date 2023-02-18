bb87ccd0ef1b77c52fd90801f1c45ceaecafffb4
ElongatedMetroid <78760239+NateNoNameSOFT@users.noreply.github.com>
2023-02-17 18:01:26 -0800

    Implement display for value, remove Value::Blob

--- chat_core/src/message.rs
+++ chat_core/src/message.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -6,10 +8,31 @@ pub enum Value {
     Integer(i64),
     Float(f64),
     Boolean(bool),
-    Blob(Vec<u8>),
+    Image(Vec<u8>),
+    File(Vec<u8>),
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Message {
+    pub username: String,
     pub payload: Value,
+}
+
+impl fmt::Display for Message {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}: {}", self.username, self.payload)
+    }
+}
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Value::String(str) => write!(f, "{str}"),
+            Value::Integer(int) => write!(f, "{int}"),
+            Value::Float(float) => write!(f, "{float}"),
+            Value::Boolean(bool) => write!(f, "{bool}"),
+            Value::Image(_) => todo!(),
+            Value::File(_) => todo!(),
+        }
+    }
 }
