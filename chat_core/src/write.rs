ad155feb43c0cc7b58df0f2ed6b35848d36dc4cd
ElongatedMetroid <78760239+NateNoNameSOFT@users.noreply.github.com>
2023-02-17 18:01:03 -0800

    add ChatBroadcaster trait which has metod to broadcast a message to a vector of TcpStreams

--- chat_core/src/write.rs
+++ chat_core/src/write.rs
@@ -3,12 +3,26 @@ use std::net::TcpStream;
 use crate::message::Message;
 
 pub trait ChatWriter {
-    fn write_message(&mut self, message: Message) -> Result<(), Box<dyn std::error::Error>>;
+    fn write_message(&mut self, message: &Message) -> Result<(), bincode::Error>;
 }
 
 impl ChatWriter for TcpStream {
-    fn write_message(&mut self, message: Message) -> Result<(), Box<dyn std::error::Error>> {
+    fn write_message(&mut self, message: &Message) -> Result<(), bincode::Error> {
         bincode::serialize_into(self, &message)?;
+
+        Ok(())
+    }
+}
+
+pub trait ChatBroadcaster {
+    fn broadcast(&mut self, message: &Message) -> Result<(), bincode::Error>;
+}
+
+impl ChatBroadcaster for Vec<TcpStream> {
+    fn broadcast(&mut self, message: &Message) -> Result<(), bincode::Error> {
+        for client in self {
+            client.write_message(&message)?;
+        }
 
         Ok(())
     }
