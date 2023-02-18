7ff851c5087a2e7df795d3b3ff19489159ac39e1
ElongatedMetroid <78760239+NateNoNameSOFT@users.noreply.github.com>
2023-02-17 18:00:22 -0800

    Return bincode::Error instead of Box<dyn std::error::Error>

--- chat_core/src/read.rs
+++ chat_core/src/read.rs
@@ -3,11 +3,11 @@ use std::net::TcpStream;
 use crate::message::Message;
 
 pub trait ChatReader {
-    fn read_message(&mut self) -> Result<Message, Box<dyn std::error::Error>>;
+    fn read_message(&mut self) -> Result<Message, bincode::Error>;
 }
 
 impl ChatReader for TcpStream {
-    fn read_message(&mut self) -> Result<Message, Box<dyn std::error::Error>> {
+    fn read_message(&mut self) -> Result<Message, bincode::Error> {
         let message: Message = bincode::deserialize_from(self)?;
 
         Ok(message)
