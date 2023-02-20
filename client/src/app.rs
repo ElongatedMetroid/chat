86d8d3d202ce190f705d45d8f708934b0e09d09c
ElongatedMetroid <78760239+NateNoNameSOFT@users.noreply.github.com>
2023-02-20 09:05:14 -0800

    Move the checking for new messages to a seperate function

--- client/src/app.rs
+++ client/src/app.rs
@@ -1,6 +1,7 @@
 use std::{
     io,
     net::TcpStream,
+    process,
     sync::{Arc, Mutex},
     thread,
     time::Duration,
@@ -11,7 +12,7 @@ use chat_core::{
     read::ChatReader,
     write::ChatWriter,
 };
-use egui::{CentralPanel, Key, Modifiers, ScrollArea, TextEdit, Window};
+use egui::{CentralPanel, Key, Modifiers, ScrollArea, TextEdit, Window, Response};
 
 pub struct App {
     client: Arc<Mutex<TcpStream>>,
@@ -30,27 +31,12 @@ impl App {
         thread::spawn({
             let client = Arc::clone(&client);
             let messages = Arc::clone(&messages);
-            move || loop {
-                thread::sleep(Duration::from_millis(500));
-                let message = client.lock().unwrap().read_data();
-                let message = match message {
-                    Ok(message) => message,
-                    Err(error) => match *error {
-                        bincode::ErrorKind::Io(error) => {
-                            if error.kind() == io::ErrorKind::WouldBlock {
-                                continue;
-                            }
-                            eprintln!("Io Error: {error}");
-                            break;
-                        }
-                        _ => {
-                            eprintln!("Lost connection to server: {error}");
-                            break;
-                        }
-                    },
-                };
 
-                messages.lock().unwrap().push(message);
+            move || {
+                check_new_messages(client, messages).unwrap_or_else(|error| {
+                    eprintln!("Error checking for new messages: {error}");
+                    process::exit(1);
+                });
             }
         });
 
@@ -64,9 +50,13 @@ impl App {
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        CentralPanel::default().show(ctx, |ui| {
+        // TODO: Only request repaint when a new message arrives
+        ctx.request_repaint();
+        
+        CentralPanel::default().show(ctx, |_ui| {
             Window::new("chat1").show(ctx, |ui| {
-                ScrollArea::vertical()
+                // Messages scroll area
+                let resopnse = ScrollArea::vertical()
                     .id_source("messages")
                     .auto_shrink([false, false])
                     .max_height(ui.available_height() / 1.5)
@@ -78,35 +68,59 @@ impl eframe::App for App {
                         }
                     });
 
+                // /resopnse.
+
                 ui.separator();
 
+                // Send message text scroll area
                 let response = ScrollArea::vertical()
-                    .max_height(100.0)
+                    .max_height(5.0)
                     .show(ui, |ui| {
-                        ui.add(TextEdit::multiline(&mut self.message_text).desired_rows(4))
+                        ui.add(TextEdit::multiline(&mut self.message_text).desired_rows(2))
                     })
                     .inner;
 
-                if ui.input(|i| {
-                    i.modifiers.matches(Modifiers::SHIFT) && i.key_pressed(Key::Enter)
-                }) {
+                if ui.input(|i| i.modifiers.matches(Modifiers::SHIFT) && i.key_pressed(Key::Enter))
+                {
                     self.message_text.push('\n');
                 }
 
                 if ui.input(|i| {
-                    i.key_pressed(Key::Enter) && response.has_focus() && !i.modifiers.matches(Modifiers::SHIFT)
+                    i.key_pressed(Key::Enter)
+                        && response.has_focus()
+                        && !i.modifiers.matches(Modifiers::SHIFT)
                 }) {
+                    let message = Message {
+                        username: String::from("Joe Smoe"),
+                        payload: Value::String(self.message_text.clone()),
+                    };
+
+                    println!("{message:?}");
+
                     self.client
                         .lock()
                         .unwrap()
-                        .write_data(&Message {
-                            username: String::from("Joe Smoe"),
-                            payload: Value::String(self.message_text.clone()),
-                        })
+                        .write_data(&message)
                         .unwrap();
                     self.message_text = String::new();
                 }
             });
         });
+    }
+}
+
+fn check_new_messages(client: Arc<Mutex<TcpStream>>, messages: Arc<Mutex<Vec<Message>>>) -> Result<(), bincode::Error> {
+    loop {
+        thread::sleep(Duration::from_millis(250));
+        let message = client.lock().unwrap().read_data::<Message>();
+        messages.lock().unwrap().push(match message {
+            Ok(message) => message,
+            Err(error) => match *error {
+                bincode::ErrorKind::Io(error) if error.kind() == io::ErrorKind::WouldBlock => {
+                    continue;
+                }
+                _ => return Err(error),
+            },
+        })
     }
 }
