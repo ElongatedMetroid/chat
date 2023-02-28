744519c433800e4498b6f833924221e877635c2e
ElongatedMetroid <78760239+NateNoNameSOFT@users.noreply.github.com>
2023-02-27 18:00:06 -0800

    clean up app module and move all chat related stuff to a chat module

--- client/src/app.rs
+++ client/src/app.rs
@@ -1,49 +1,19 @@
-use std::{
-    io,
-    net::TcpStream,
-    process,
-    sync::{Arc, Mutex},
-    thread,
-    time::Duration,
-};
+use std::{process, io};
 
-use chat_core::{
-    read::ChatReader,
-    write::ChatWriter, request::{Request, Requesting}, message::Message, value::Value,
-};
-use egui::{CentralPanel, Key, Modifiers, ScrollArea, TextEdit, Window};
+use egui::CentralPanel;
+
+use crate::chat::Chat;
 
 pub struct App {
-    client: Arc<Mutex<TcpStream>>,
-    messages: Arc<Mutex<Vec<Message>>>,
-    message_text: String,
+    chat: Chat,
 }
-// TODO: Client - Gui
-// TODO: Server - Usernames
+
 impl App {
     pub fn new(
-        client: Arc<Mutex<TcpStream>>,
-        messages: Arc<Mutex<Vec<Message>>>,
         _cc: &eframe::CreationContext<'_>,
     ) -> Self {
-        client.lock().unwrap().set_nonblocking(true).unwrap();
-
-        thread::spawn({
-            let client = Arc::clone(&client);
-            let messages = Arc::clone(&messages);
-
-            move || {
-                check_new_messages(client, messages).unwrap_or_else(|error| {
-                    eprintln!("Error checking for new messages: {error}");
-                    process::exit(1);
-                });
-            }
-        });
-
         Self {
-            client,
-            messages,
-            message_text: String::new(),
+            chat: Chat::new("127.0.0.1:1234").unwrap()
         }
     }
 }
@@ -54,76 +24,37 @@ impl eframe::App for App {
         ctx.request_repaint();
 
         CentralPanel::default().show(ctx, |_ui| {
-            Window::new("chat1").show(ctx, |ui| {
-                // Messages scroll area
-                ScrollArea::vertical()
-                    .id_source("messages")
-                    .auto_shrink([false, false])
-                    .max_height(ui.available_height() / 1.5)
-                    .max_width(f32::INFINITY)
-                    .show(ui, |ui| {
-                        let messages = self.messages.lock().unwrap();
-                        for message in messages.iter() {
-                            ui.label(format!("{message}"));
-                        }
-                    });
-
-                ui.separator();
-
-                if self.message_text.bytes().len() as u64 > ChatWriter::byte_limit(&(*self.client.lock().unwrap())) {
-                    ui.label("Message Too Long!");
-                    ui.separator();
+            if let Err(error) = self.chat.check_new_messages() {
+                match *error {
+                    bincode::ErrorKind::Io(e) if e.kind() == io::ErrorKind::WouldBlock => (),
+                    _ => {
+                        eprintln!("Error checking for new messages: {error}");
+                        process::exit(1);
+                    }
                 }
+            }
+            self.chat.update(&ctx);
+            // if !self.config_handled {
+            //     match config {
 
-                // Send message text scroll area
-                let response = ScrollArea::vertical()
-                    .max_height(5.0)
-                    .show(ui, |ui| {  
-                        ui.add(TextEdit::multiline(&mut self.message_text).desired_rows(2))
-                    })
-                    .inner;
+            //     }
+            //     match self.config.username() {
+            //         // Username is set
+            //         Some(username) => {
 
-                if ui.input(|i| i.modifiers.matches(Modifiers::SHIFT) && i.key_pressed(Key::Enter))
-                {
-                    self.message_text.push('\n');
-                }
+            //         },
+            //         // Username is not set, and is not set to be random everytime
+            //         None if !self.config.username().random_username() => {
 
-                if ui.input(|i| {
-                    i.key_pressed(Key::Enter)
-                        && response.has_focus()
-                        && !i.modifiers.matches(Modifiers::SHIFT)
-                }) {
-                    
+            //         }
+            //         // Username is set to be random everytime
+            //         None if self.config.username().random_username() => {
 
-                    let request = Request::builder()
-                        .requesting(Requesting::SendMessage)
-                        .payload(Value::String(self.message_text.trim_end().to_owned()))
-                        .build();
-
-                    self.client.lock().unwrap().write_data(&request).unwrap();
-                    
-                    self.message_text.clear();
-                }
-            });
+            //         }
+            //     }
+            // } else {
+                
+            // }
         });
-    }
-}
-
-fn check_new_messages(
-    client: Arc<Mutex<TcpStream>>,
-    messages: Arc<Mutex<Vec<Message>>>,
-) -> Result<(), bincode::Error> {
-    loop {
-        thread::sleep(Duration::from_millis(250));
-        let message = client.lock().unwrap().read_data::<Message>();
-        messages.lock().unwrap().push(match message {
-            Ok(message) => message,
-            Err(error) => match *error {
-                bincode::ErrorKind::Io(error) if error.kind() == io::ErrorKind::WouldBlock => {
-                    continue;
-                }
-                _ => return Err(error),
-            },
-        })
     }
 }
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,20 +1,12 @@
-use std::{
-    net::TcpStream,
-    sync::{Arc, Mutex},
-};
-
 use client::app::App;
 use eframe::{run_native, NativeOptions};
 
 fn main() {
     let native_options = NativeOptions::default();
-    let messages = Arc::new(Mutex::new(Vec::new()));
-    let client = Arc::new(Mutex::new(TcpStream::connect("127.0.0.1:1234").unwrap()));
-
     run_native(
         "chat",
         native_options,
-        Box::new(|cc| Box::new(App::new(client, messages, cc))),
+        Box::new(|cc| Box::new(App::new(cc))),
     )
     .unwrap();
 }
